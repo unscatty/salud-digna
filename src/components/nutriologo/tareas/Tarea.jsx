c4472ba2aab020a0d0f85b22206565d11e39cb1c
Aaron Rincon <rlaaron0305@gmail.com>
2023-04-12 22:09:14 -0600

    Subo nutriologo

--- src/components/nutriologo/tareas/Tarea.jsx
+++ src/components/nutriologo/tareas/Tarea.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Tarea() {
   return (
-    <div className='aspect-square h-100% bg-second text-black'>Tarea</div>
+    <div className='aspect-square h-100% bg-second text-black rounded-xl p-2 text-center font-semibold flex flex-col justify-center item-center'>
+      Aprovar Plan
+      <img src="/public/assets/frutas.svg" alt="" />
+    </div>
   )
 }
 
--- src/pages/nutriologo.jsx
+++ src/pages/nutriologo.jsx
@@ -7,33 +7,33 @@ export default function nutriologo() {
       <nav className="fixed l-0 t-0 h-100% bg-third w-20 py-10">
         <div className="container-nutriologo__logo bg-white rounded-full  w-50% m-auto flex flex-col justify-between h-50%">
           <img
-            src="/src/assets/logo2.svg"
+            src="/public/assets/logo2.svg"
             alt="logo"
             className="rounded-full"
           />
           <img
-            src="/src/assets/logo2.svg"
+            src="/public/assets/logo2.svg"
             alt="logo"
             className="rounded-full"
           />
           <img
-            src="/src/assets/logo2.svg"
+            src="/public/assets/logo2.svg"
             alt="logo"
             className="rounded-full"
           />
           <img
-            src="/src/assets/logo2.svg"
+            src="/public/assets/logo2.svg"
             alt="logo"
             className="rounded-full"
           />
           <img
-            src="/src/assets/logo2.svg"
+            src="/public/assets/logo2.svg"
             alt="logo"
             className="rounded-full"
           />
         </div>
       </nav>
-      <div className="container-nutriologo__content grid gap-10 p-10 grid-rows-[auto_1fr_1fr_1fr] ml-20 h-100vh">
+      <div className="container-nutriologo__content grid gap-10 p-10 grid-rows-[auto_1fr_1fr_1fr] ml-20 h-100vh grid-cols-[2fr_1fr] ">
         <div className="container-nutriologo__content  bg-third w-full rounded p-4 col-span-3">
           <p className="text-black font-bold text-2xl">Buenos Dias Juan</p>
           <p className="font-bold text-gray">
@@ -42,7 +42,7 @@ export default function nutriologo() {
         </div>
 
         <div className="tareas col-span-2 bg-third rounded p-4 grid grid-rows-[auto_1fr] gap-4">
-          <p className="text-black font-semibold">Tareas a realizar</p>
+          <p className="text-black font-bold  text-xl ">Tareas a realizar</p>
 
           <NutriologoTareas />
         </div>
@@ -54,14 +54,27 @@ export default function nutriologo() {
           </div>
         </div>
         <div className="clientes row-span-2 bg-white rounded text-black grid grid-rows-[auto_1fr] gap-4 p-4">
-          <p className='text-xl font-bold'>Clientes</p>
+          <p className="text-xl font-bold">Clientes</p>
           <div>
             <NutriologoCliente />
           </div>
         </div>
-        <div className="grafica bg-third rounded p-4 text-black font-bold  "><p className='text-xl'>Satisfaccion de clientes</p></div>
-        <div className="grafica bg-third rounded p-4 text-black"><p className='text-xl font-bold'>Progreso de clientes</p></div>
-        <div className="grafica bg-third rounded p-4 text-black ">Mensajes</div>
+
+        <div
+          className="grafica bg-third rounded p-4 text-black 
+        row-span-2"
+        >
+          <p className="text-xl font-bold">Mensajes</p>
+          
+        </div>
+        <div className='grid grid-cols-[1fr_1fr] gap-10'>
+          <div className="grafica bg-third rounded p-4 text-black font-bold  ">
+            <p className="text-xl">Satisfaccion de clientes</p>
+          </div>
+          <div className="grafica bg-third rounded p-4 text-black">
+            <p className="text-xl font-bold">Progreso de clientes</p>
+          </div>
+        </div>
       </div>
     </div>
   );
